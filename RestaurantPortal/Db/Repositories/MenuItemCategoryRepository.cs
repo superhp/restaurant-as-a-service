52f78e0d419949520bd53c54380a295a283d6c71
Laurynas <laurynas@simkunas.lt>
2018-10-13 00:31:35 +0300

    Get categories API endpoint

---
+++ RestaurantPortal/Db/Repositories/MenuItemCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RestaurantPortal.Models;
+
+namespace RestaurantPortal.Db.Repositories
+{
+    public class MenuItemCategoryRepository : IMenuItemCategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MenuItemCategoryRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<MenuItemCategoryDto> GetAll()
+        {
+            var categories = _dbContext.MenuItemCategories.Select(c => new MenuItemCategoryDto
+            {
+                Id = c.MenuItemCategoryId,
+                Name = c.Name
+            });
+
+            return categories;
+        }
+    }
+}
--- RestaurantPortal/RestaurantPortal/Startup.cs
+++ RestaurantPortal/RestaurantPortal/Startup.cs
@@ -41,6 +41,7 @@ namespace RestaurantPortal
             });
 
             services.AddScoped<IMenuRepository, MenuRepository>();
+            services.AddScoped<IMenuItemCategoryRepository, MenuItemCategoryRepository>();
 
             var builder = new ContainerBuilder();
             builder.Populate(services);
