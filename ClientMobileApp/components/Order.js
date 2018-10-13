da861f2bb4aa14291e4cf41c34f40ce5a0d1db12
Bončkus Artūras <arturas.bonckus@mif.stud.vu.lt>
2018-10-13 13:45:54 +0300

    Order - selecting items

--- ClientMobileApp/components/Order.js
+++ ClientMobileApp/components/Order.js
@@ -46,74 +46,59 @@ export default class Order extends React.Component {
         this.setState({ loading: false });
     }
 
-    addItem(id, name, quantity) {
-        const items = this.state.order;
-        if(items.indexOf(x => x.id == id) > -1) {
-            var item = items.find(i => i.id == id);
+    addItem(id, quantity) {
+        let items = this.state.menu;
+        if (items.find(i => i.menuItemId === id)) {
+            let item = items.find(i => i.menuItemId === id);
             item.quantity += quantity;
-            items[items.indexOf(x => x.id == id)] = item;
+            //items[items.indexOf(x => x.menuItemId === id)] = item;
         }
-        else {
-            items.push({id: id, name: name, quantity: quantity});
-        }        
-        this.setState({order: items})
+        this.setState({ menu: items })
     }
 
     render() {
         const footerVisible = this.state.order !== 0;
-        const items = this.state.categories.map(c => this.state.menu.filter(i => i.categoryId === c.id));
-        console.log(items)
 
-        if (this.state.loading || (typeof this.state.menu == 'undefined' && this.state.menu == 0) || this.state.categories == 0) {
+        if (this.state.loading || (typeof this.state.menu === 'undefined' && this.state.menu === 0) || this.state.categories === 0) {
             return (
                 <Container>
                     <Spinner color='blue' />
                 </Container>
             );
         }
-
-        return (
+        console.log(this.state.menu)
+        let items = this.state.menu;
+        return (            
             <Container>
                 <Header />
                 <Content>
-                    {this.state.categories.map(c => {
-                        const items = this.state.menu.filter(i => i.categoryId === c.id);
-                        items.unshift({ name: c.name, header: true });
-                        return <List key={c.id} dataArray={items}
-                            renderRow={(item) => {
-                                if(item.header)
-                                    return(
-                                        <ListItem itemHeader first>
-                                            <Text>{item.name}</Text>
-                                        </ListItem>
-                                    );
-                                
-                                return <ListItem thumbnail>
-                                    <Left>
-                                        <Thumbnail square large source={{ uri: item.image }} />
-                                    </Left>
-                                    <Body>
-                                        <Text>{item.name}</Text>
-                                        <Text note numberOfLines={1}> </Text>
-                                        <Text note style={styles.price} numberOfLines={1}>{item.price}</Text>
-                                    </Body>
-                                    <Right>
-                                        <Button transparent onPress={this.function_call}>
-                                            <Text style={styles.selectionButtons}>-</Text>
-                                        </Button>
-                                    </Right>
-                                    <Right>
-                                        <Text style={styles.itemCount}>{getItemQuantity(item.menuItemId)}</Text>
-                                    </Right>
-                                    <Right>
-                                        <Button transparent onPress={this.addItem(item.menuItemId, item.name, 1)}>
-                                            <Text style={styles.selectionButtons}>+</Text>
-                                        </Button>
-                                    </Right>
-                                </ListItem>
-                            }}>                            
-                        </List>
-                    })}
+                    <List dataArray={items}
+                        renderRow={(item) =>
+                            <ListItem thumbnail>
+                                <Left>
+                                    <Thumbnail square large source={{ uri: item.image }} />
+                                </Left>
+                                <Body>
+                                    <Text>{item.name}</Text>
+                                    <Text note numberOfLines={1}> </Text>
+                                    <Text note style={styles.price} numberOfLines={1}>{item.price}</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Text style={styles.selectionButtons}>-</Text>
+                                    </Button>
+                                </Right>
+                                <Right>
+                                    <Text style={styles.itemCount}>{}</Text>
+                                </Right>
+                                <Right>
+                                    <Button transparent onPress={() => this.addItem(item.menuItemId, 1)}>
+                                        <Text style={styles.selectionButtons}>+</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                        }>
+                    </List>
                 </Content>
                 <OrderTotal hide={footerVisible}>
                     <FooterTab>
