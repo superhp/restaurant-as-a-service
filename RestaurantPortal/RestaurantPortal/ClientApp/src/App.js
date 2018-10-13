ee17c6e943948b4d4501387964a441ee51b985dd
Aurimas Šimkus <saurimas@hotmail.com>
2018-10-13 04:13:36 +0300

    Initial login page

--- RestaurantPortal/RestaurantPortal/ClientApp/src/App.js
+++ RestaurantPortal/RestaurantPortal/ClientApp/src/App.js
@@ -1,14 +1,16 @@
 ﻿import React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import Login from './components/Login';
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+	<Switch>
+		<Route path='/' component={Login} />
+		<Layout>    
+			<Route exact path='/manage' component={Home} />
+		</Layout>
+	</Switch>
 );
