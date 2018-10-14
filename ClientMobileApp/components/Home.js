d8a9358c9d269f831f56abd7c0e1a4149b22a5ad
Bončkus Artūras <arturas.bonckus@mif.stud.vu.lt>
2018-10-14 10:27:58 +0300

    Home page and theming

--- ClientMobileApp/components/Home.js
+++ ClientMobileApp/components/Home.js
@@ -1,46 +1,54 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Right, Button, Body, Title, Icon } from 'native-base';
+import { Container, Header, Left, Right, Button, Body, Title, Text, Content } from 'native-base';
+import Carousel from 'react-native-carousel';
 import Expo from "expo";
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 export default class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { loading: true};
+        this.state = { loading: true };
     }
     async componentWillMount() {
         await Expo.Font.loadAsync({
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
         });
         this.setState({ loading: false });
-      }
+    }
 
     componentDidMount() {
         StatusBar.setHidden(true);
     }
 
     render() {
-        return this.state.loading ? null :(
-            <Container>
-                <Header>
-                    <Left>
-                        <Button transparent>
-                        <Icon name='arrow-back' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Header</Title>
-                    </Body>
-                    <Right>
-                        <Button transparent>
-                        <Icon name='menu' />
-                        </Button>
-                    </Right>
-                </Header>
+        return this.state.loading ? null : (
+            <Container style={styles.container}>
+                <Content>
+                    <Carousel>
+                        <View>
+                            <Text>Page 1</Text>
+                        </View>
+                        <View>
+                            <Text>Page 2</Text>
+                        </View>
+                        <View>
+                            <Text>Page 3</Text>
+                        </View>
+                    </Carousel>
+                </Content>
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#002b47'
+    },
+    headerText: {
+        color: '#ff6600'
+    }
+});
