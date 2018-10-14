9c3d1a3413861aa059161ca2a4791ae0baa2dd51
Bončkus Artūras <arturas.bonckus@mif.stud.vu.lt>
2018-10-14 13:02:37 +0300

    Removed labels and added structure for restaurant list

---
+++ ClientMobileApp/components/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'native-base';
+
+export default class Settings extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
